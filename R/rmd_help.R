6d4ed497a8cf67785b19ce4c1739c76493b0115e
Miles McBain <miles.mcbain@gmail.com>
2021-06-14 22:47:30 +1000

    remove extra "." in helpfile name

--- R/rmd_help.R
+++ R/rmd_help.R
@@ -21,5 +21,5 @@ rmd_help <- function(topic) {
   md_help <- system2("pandoc", c("-f", "html", "-t", "markdown+multiline_tables", "--columns=80", my_tmp), stdout = TRUE)
   examples_line <- which(grepl("#+\\sExamples", md_help))
   rmd_help <- c(md_help[1:examples_line + 1], "```{r}", md_help[(examples_line + 2):length(md_help)], "```")
-  rstudioapi::documentNew(text = paste0(rmd_help, collapse = "\n"), type = ".help.Rmd")
+  rstudioapi::documentNew(text = paste0(rmd_help, collapse = "\n"), type = "help.Rmd")
 }
