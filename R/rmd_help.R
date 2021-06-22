a66eac36fff38b3c6a29855798d85a6381a1e551
Miles McBain <miles.mcbain@qfes.qld.gov.au>
2021-06-22 12:16:16 +1000

    add help file title and wrap usage in chunk

--- R/rmd_help.R
+++ R/rmd_help.R
@@ -14,13 +14,13 @@ rmd_help <- function(topic) {
   the_topic <- deparse(substitute(topic))
   help_matches <- utils::help(the_topic)
   if (length(help_matches) < 1) stop("Couldn't find help for ", as.character(the_topic))
-  
+
   help_file <- help_matches[[1]]
   help_file_name <- fs::path_file(help_file)
   pkg_user_dir <- get_pkg_user_dir()
   target_file_name <- paste0(help_file_name, "_help.rmd")
   target_file <- fs::file_create(file.path(pkg_user_dir, target_file_name))
-   
+
   x <- tools::Rd2txt(
     utils:::.getHelpFile(help_file),
     out = target_file,
@@ -44,15 +44,34 @@ rmd_help <- function(topic) {
 
   examples_line <- which(grepl("###\\sExamples", md_help))
   if (any(examples_line)) {
-    rmd_help <- c(md_help[1:examples_line + 1], "```{r}", md_help[(examples_line + 2):length(md_help)], "```")
+    rmd_help <- c(
+      md_help[1:examples_line + 1],
+      "```{r}",
+      md_help[(examples_line + 2):length(md_help)],
+      "```"
+    )
   } else {
     rmd_help <- md_help
   }
+  usage_line <- which(grepl("###\\sUsage", rmd_help))
+  if (any(usage_line)) {
+    headings <- which(grepl("^###", rmd_help))
+    usage_end_line <- min(headings[headings > usage_line]) - 2
+    rmd_help <- c(
+      rmd_help[1:usage_line + 1],
+      "```{r}",
+      rmd_help[(usage_line + 2):usage_end_line],
+      "```",
+      rmd_help[(usage_end_line + 1):length(rmd_help)]
+    )
+  }
+
+  rmd_help <- c(paste0("# ", help_file_name), "", rmd_help)
   readr::write_file(paste0(rmd_help, collapse = "\n"), target_file)
   rstudioapi::navigateToFile(target_file)
 }
 
-get_pkg_user_dir <- function(){
+get_pkg_user_dir <- function() {
   rmdocs_user_dir <- tools::R_user_dir("rmdocs")
   if (!dir.exists(rmdocs_user_dir)) {
     dir.create(rmdocs_user_dir, recursive = TRUE)
